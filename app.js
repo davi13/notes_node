70226021da843c7f51ffab73f6955aa30a46bd13
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-01 12:28:14 +0200

    initizlized the note project with node

---
+++ app.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk');
+const getNotes = require('./notes');
+const yargs = require('yargs');
+const command = process.argv[2];
+console.log(process.argv)
+console.log(yargs.argv);
+
+
---
+++ utils.js
@@ -0,0 +1,4 @@
+console.log('utils.js')
+const name = 'lol';
+
+module.exports = name;
