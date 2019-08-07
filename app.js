8cb5b7befff47f2eb7e6ff70aac74ce9a25c1b65
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-08 00:54:19 +0200

    star using our  Json in the app

--- app.js
+++ app.js
@@ -7,35 +7,50 @@ yargs.version('1.1.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note')
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'note body',
+            demandOption: true,
+            type: 'string'
+        }
+
+    },
+    handler: function (argv) {
+        console.log('Title:' + argv.title);
+        console.log('description:' + argv.body);
     }
 })
-yargs.command({
-    command: 'remove',
-    describe: 'Removing a note',
-    handler: () => {
-        console.log('Removing a note');
-    }
-})
+// yargs.command({
+//     command: 'remove',
+//     describe: 'Removing a note',
+//     handler: () => {
+//         console.log('Removing a note');
+//     }
+// })
 
-yargs.command({
-    command: 'list',
-    describe: 'Listing a note',
-    handler: () => {
-        console.log('listing out all the note');
-    }
+// yargs.command({
+//     command: 'list',
+//     describe: 'Listing a note',
+//     handler: () => {
+//         console.log('listing out all the note');
+//     }
 
-})
+// })
 
-yargs.command({
-    command: 'read',
-    describe: 'Reading all the notes',
-    handler: () => {
-        console.log('Reading all the note');
-    }
-})
-console.log(process.argv)
-console.log(yargs.argv);
+// yargs.command({
+//     command: 'read',
+//     describe: 'Reading all the notes',
+//     handler: () => {
+//         console.log('Reading all the note');
+//     }
+// })
 
 
+
+
+//yargs.parse();
