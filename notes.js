a680f1779ee7584f379f9ea04de16ea475783547
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-13 08:30:13 +0200

    added the loadNotes function to node.js file

--- notes.js
+++ notes.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
+
 const getNotes = () => {
     return 'your notes...';
 }
-const addNote = (title, body){
-
+const addNote = function (title, body) {
+    const notes = loadNotes()
+}
+const loadNotes = function () {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJson = dataBuffer.toString();
+    return JSON.parse(dataJson);
 }
 module.exports = {
     getNotes: getNotes,
-    addNote: addNote
+    addNote: addNote,
+    loadNotes: loadNotes
 }
