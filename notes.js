83cdbd8f1652705c01e85afd22ded3b350c934fe
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-13 08:55:56 +0200

    added a method that avoid duplicate notes with the method filter

--- notes.js
+++ notes.js
@@ -5,12 +5,18 @@ const getNotes = () => {
 }
 
 const addNote = function (title, body) {
-    const notes = loadNotes()
-    notes.push({
-        title: title,
-        body: body
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title;
     });
-    saveNotes(notes);
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        });
+        saveNotes(notes);
+    }
+
 }
 
 const saveNotes = function (notes) {
