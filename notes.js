4c3631c3dd2ebe190fb71085b59874604f37bc72
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-13 10:53:59 +0200

    fixed some bug find in the removeNotes function

--- notes.js
+++ notes.js
@@ -1,62 +1,59 @@
-const fs = require('fs');
+const fs = require('fs')
+const chalk = require('chalk')
 
-const getNotes = () => {
-    return 'your notes...';
+const getNotes = function () {
+    return 'Your notes...'
 }
 
 const addNote = function (title, body) {
-    const notes = loadNotes();
+    const notes = loadNotes()
     const duplicateNotes = notes.filter(function (note) {
-        return note.title === title;
-    });
+        return note.title === title
+    })
+
     if (duplicateNotes.length === 0) {
         notes.push({
             title: title,
             body: body
-        });
-        saveNotes(notes);
-        console.log('New notes added ! ');
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added!'))
     } else {
-        console.log('Note title taken ');
+        console.log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const removeNote = function (title) {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.red.inverse('No note found!'))
     }
 }
 
 const saveNotes = function (notes) {
-    const dataJson = JSON.stringify(notes);
-    fs.writeFileSync('notes.json', dataJson);
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
 const loadNotes = function () {
     try {
-        const dataBuffer = fs.readFileSync('notes.json');
-        const dataJson = dataBuffer.toString();
-        return JSON.parse(dataJson);
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
     } catch (e) {
-        return [];
+        return []
     }
-
 }
-const removeNote = function (title) {
-    const notes = loadNotes();
-    const result = notes.filter(function (note) {
-        return note.title === title
-    });
-    console.log('my result sir ', result);
 
-
-    // notes.filter(function (note) {
-    //     console.log(note.title)
-    // return note.title === title;
-
-    // })
-
-
-
-
-}
 module.exports = {
     getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote
-    // loadNotes: loadNotes
 }
