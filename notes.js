be8a3d22105b5ddbfeecff108c07ea9bfd2b88ea
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-15 19:03:09 +0200

    notes.js: using find instead of filter

--- notes.js
+++ notes.js
@@ -7,7 +7,8 @@ const getNotes = () => {
 
 const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter((note) => note.title === title)
+    const duplicateNotes = notes.filter((note) => note.title === title);
+    const duplicateNote = notes.find((note) => note.title === title);
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -49,7 +50,6 @@ const loadNotes = () => {
 }
 const listNotes = () => {
     const notes = loadNotes();
-    //console.log(notes);
     notes.forEach((note) => {
         console.log(chalk.inverse.blue(note.title));
     });
