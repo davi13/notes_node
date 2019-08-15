3c38b018d3b625c79cb784a8219fbdf5bffd2af7
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-15 23:49:47 +0200

    notes.js: quitting debugger

--- notes.js
+++ notes.js
@@ -6,6 +6,7 @@ const chalk = require('chalk')
 const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.title === title);
+
     debugger;
 
     if (!duplicateNote) {
@@ -65,7 +66,6 @@ const listNotes = () => {
 
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
